509bcc9e953072f7c779549602c41d43f5ae33b6
swanner <1060742052@qq.com>
2019-12-11 00:41:08 +0800

    del
    Signed-off-by: swanner <1060742052@qq.com>

---
+++ frontend/src/views/form/components/d-amend.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <el-dialog title="edit chatroom" :visible.sync="isShow">
+      <el-form :model="form">
+        <el-form-item label="room type" label-width="100px">
+          <el-select v-model="form.room_type" placeholder="type">
+            <el-option label="star" :value="1"></el-option>
+            <el-option label="net" :value="2"></el-option>
+          </el-select>
+        </el-form-item >
+        <el-form-item label="room name" label-width="100px">
+          <el-input v-model="form.room_name"></el-input>
+        </el-form-item>
+
+        <el-form-item label="people limit" label-width="100px">
+          <el-input v-model.number="form.people_limit"></el-input>
+        </el-form-item>
+        <el-form-item label="room desc" label-width="100px">
+          <el-input v-model="form.room_desc"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer">
+        <el-button @click="_hide">cancel</el-button>
+        <el-button type="primary" @click="handleSubmit">confirm</el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import { amendChatRoom } from '@/api/chatRoom.js'
+export default {
+  data() {
+    return {
+      isShow: false,
+      form: {
+        id: null,
+        room_name: '',
+        room_type: null,
+        room_desc: '',
+        people_limit: null
+      }
+    }
+  },
+  methods: {
+    _show(id) {
+      this.form.id = id
+      this.isShow = true
+    },
+    _hide(isChange) {
+      this.isShow = false
+      this.form = []
+      if (isChange == true) {
+        this.$emit('update')
+      }
+    },
+    handleSubmit() {
+      amendChatRoom(this.form).then(res => {
+        if (res.code === 2000) {
+          this.$message.success(res.msg)
+        } else {
+          this.$message.error(res.msg)
+        }
+        this._hide(true)
+      })
+    }
+  }
+}
+</script>
