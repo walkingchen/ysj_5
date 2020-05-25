6ad9b6cdd3be609834229c4cf437dce5b2d71592
tiantian <tiantian@sanlogic.com>
2020-05-25 15:42:53 +0800

    Room management

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,6 +5,10 @@ import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import Icon from 'vue-awesome/components/Icon'
+import lang from 'element-ui/lib/locale/lang/en'
+import locale from 'element-ui/lib/locale'
+
+locale.use(lang)
 
 Vue.config.productionTip = false
 
