d3aa93f861e2520a1b51d95d9b334023e62540d3
tiantian <abugui_tian@163.com>
2021-07-11 16:47:57 +0800

    默认显示两条评论和评论输入框； topic of the day不显示用户信息，显示标题和摘要； 修复可以多次like的bug.

--- frontend/src/views/room/publicTimeline.vue
+++ frontend/src/views/room/publicTimeline.vue
@@ -1,26 +1,5 @@
 <template>
   <div class="publicTlimeline-layout">
-    <el-card class="post-create-layout">
-      <textarea rows="5" class="post-create-content" placeholder="What's on your mind?" v-model="postContent" />
-      <div v-if="showPostImage" class="img-box" v-loading="postImageLoading">
-        <span class="delete-btn" @click="deletePostImage">&times;</span>
-        <img v-if="postImageUri" :src="postImageUri" />
-      </div>
-      <el-button type="text" size="mini" @click="selectFile"><v-icon name="images" /></el-button>
-      <input ref="fileInput" type="file" hidden accept="image/*" @change="selectedImage" />
-      <el-button class="post-create-btn" type="primary" size="mini" :loading="submitPostLoading" @click="submitPost">Post</el-button>
-    </el-card>
-
-    <el-card class="topic-layout">
-      <title-com title="Topic of The Day" />
-      <public-post-item
-        v-for="item in topicList"
-        :key="item.id"
-        :item="formatPostData(item, false)"
-        :is-topic="true"
-        @action-success="updatePost" />
-    </el-card>
-
     <div class="new-tip" v-show="newCount > 0">
       {{ newCount }} new messages, click <a href="javascript:;" @click="getNews">here</a> to update.
     </div>
@@ -42,39 +21,22 @@
 
 <script>
 import { mapState } from 'vuex'
-import 'vue-awesome/icons/images'
-import {
-  getTopicContent,
-  getPosts,
-  getPost,
-  postPhoto,
-  createPost
-} from '@api/post'
-import titleCom from '@components/title'
+import { getPosts, getPost } from '@api/post'
 import publicPostItem from '@components/publicPostItem'
 import { formatDate } from '@assets/utils.js'
 
 export default {
-  props: ['sid'],
   data() {
     return {
-      postContent: '',
-      showPostImage: false,
-      postImageLoading: false,
-      postImageUri: '',
-      postImageFileName: '',
-      topicList: [],
       getPostLoading: true,
       moments: [],
       noMoreData: false,
-      submitPostLoading: false,
       me_post_moments: [],
       newCount: 0,
       getNewPostLoading: false
     }
   },
   components: {
-    titleCom,
     publicPostItem
   },
   computed: {
@@ -92,43 +54,10 @@ export default {
   },
   created() {
     this.$bus.$on('share-success', id => {
-      this.updatePost({
-        id,
-        type: 0
-      })
+      this.updatePost(id, 0)
     })
   },
   methods: {
-    selectFile () {
-      this.$refs.fileInput.click()
-    },
-    selectedImage (e) {
-      const file = e.target.files[0]
-      const fileExt = file.name.split('.').pop().toLocaleLowerCase()
-      if (['png', 'jpg', 'jpeg', 'bmp', 'gif', 'webp', 'psd', 'svg', 'tiff'].includes(fileExt)) {
-        this.showPostImage = true
-        this.postImageLoading = true
-
-        const fileForm = new FormData()
-        fileForm.append('file', file)
-        postPhoto(fileForm).then(({ data }) => {
-          if (data.result_code === 2000) {
-            this.postImageLoading = false
-            this.postImageUri = data.data.upload_path + data.data.filename_s
-            this.postImageFileName = data.data.filename
-          }
-        })
-      } else {
-        this.$message.warning('Please select a image.')
-      }
-
-      this.$refs.fileInput.value = ''
-    },
-    deletePostImage () {
-      this.postImageUri = ''
-      this.postImageFileName = ''
-      this.showPostImage = false
-    },
     formatPostData (item, showShared = true) {
       const user = this.members.find(ele => ele.id === item.user_id)
       const _item = {
@@ -152,27 +81,13 @@ export default {
           avatar: null,
           nickname: ''
         },
-        comments: item.comments.map(ele => {
-          ele.created_at = formatDate(ele.created_at)
-          return ele
-        })
+        comments: item.comments
       }
 
       if (_item.isShared) {
-        _item.postSource = {
-          id: item.post_shared_id,
-          time: formatDate(item.post_shared.created_at),
-          title: item.post_shared.post_title,
-          content: item.post_shared.post_content,
-          photo_uri: item.post_shared.photo_uri
-        }
+        _item.postSource = item.post_shared
       }
       return _item
-    },
-    updateTopic () {
-      getTopicContent(localStorage.getItem('roomid'), this.currentTopic).then(({ data }) => {
-        this.topicList = data.data
-      })
     },
     async getMomentList() {
       this.getPostLoading = true
@@ -206,67 +121,28 @@ export default {
       })
       this.getNewPostLoading = false
     },
-    updatePost ({ id, type = 1, isTopic = false }) {
+    updatePost (id, type = 1) {
       /*
       * type = 1 更新已经获取过的timeline，用来进行了like/dislike/comment/fackcheck操作后对当前单条timeline进行刷新
       * type = 0 用来分享了private timeline或新增了一条timeline后将这条timeline直接显示在最上方而不用获取所有新的post
       */
-      if (type && isTopic) {
-        this.updateTopic()
-      } else {
-        getPost(id).then(res => {
-          if (type) {
-            const momentIndex = this.moments.findIndex(ele => ele.id === id)
-            this.moments.splice(momentIndex, 1, res.data.data)
-          } else {
-            this.$bus.$emit('share-success-refresh', id)
-            setTimeout(() => {
-              this.me_post_moments.unshift(res.data.data)
-            }, 1000)
-          }
-        })
-      }
-    },
-    async submitPost() {
-      if (this.postContent) {
-        this.submitPostLoading = true
-
-        const params = {
-          post_content: this.postContent,
-          timeline_type: 0,
-          post_type: 1,
-          topic: this.currentTopic,
-          sid: this.sid,
-          room_id: Number(localStorage.getItem('roomid'))
+      getPost(id).then(res => {
+        if (type) {
+          const momentIndex = this.moments.findIndex(ele => ele.id === id)
+          this.moments.splice(momentIndex, 1, res.data.data)
+        } else {
+          this.$bus.$emit('share-success-refresh', id)
+          setTimeout(() => {
+            this.me_post_moments.unshift(res.data.data)
+          }, 1000)
         }
-        if (this.postImageFileName) {
-          params.photo_uri = this.postImageFileName
-        }
-
-        await createPost(params).then(res => {
-          this.postContent = ''
-
-          this.updatePost({
-            id: res.data.data.id,
-            type: 0
-          })
-        })
-        this.submitPostLoading = false
-      } else {
-        this.$message({
-          message: 'Please enter the content.',
-          type: 'warning'
-        })
-      }
+      })
     }
   },
   watch: {
     currentTopic: {
       handler (topic) {
         if (topic) {
-          this.topicList = []
-          this.updateTopic()
-
           this.me_post_moments = []
           this.moments = []
           this.getMomentList()
@@ -280,59 +156,6 @@ export default {
 
 <style lang="stylus">
 .publicTlimeline-layout
-  .post-create-layout
-    border 0
-    padding 15px 10px 10px
-
-    .post-create-content
-      width 100%
-      border 0
-      resize none
-      outline none
-
-    .img-box
-      width 100px
-      height 100px
-      display flex
-      justify-content center
-      align-items center
-      border 1px solid #ebeef5
-      border-radius 4px
-      padding 3px
-      position relative
-
-      .delete-btn
-        display none
-        position absolute
-        top -5px
-        right -5px
-        width 16px
-        height 16px
-        border-radius 8px
-        border 1px solid #eee
-        background-color #fff
-        line-height 16px
-        text-align center
-        cursor pointer
-        color #777
-
-        &:hover
-          color #444
-
-      &:hover .delete-btn
-        display block
-
-      img
-        max-width 100%
-        max-height 100%
-
-    .post-create-btn
-      float right
-
-  .topic-layout
-    border 0
-    margin-top 20px
-
   .new-tip
     background-color #fff
     margin-top 20px
