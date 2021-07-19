b016582fb51c71904ff7c4adc1f233e718cc36a9
CodingChan <codingchan@gmail.com>
2021-07-19 14:09:06 +0800

    test reload
    Signed-off-by: CodingChan <codingchan@gmail.com>

--- y5-backend-flask/app.py
+++ y5-backend-flask/app.py
@@ -79,7 +79,6 @@ def reload_vue():
 def reload():
     if request.method == 'POST':
         # json = request.args()
-
         # if json['sender']['login'] == 'codingchan':
         git_pull()
         
