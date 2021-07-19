68ed1b00c9df88bcf6226fee478454d975cdd8a4
tiantian <tiantian@sanlogic.com>
2021-07-19 11:08:39 +0800

    禁止手机端显示(ISS-109)

---
+++ frontend/src/views/Phone.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="phone-layout">
+    Please open on PC or Pad.
+  </div>
+</template>
+
+<style lang="stylus" scoped>
+.phone-layout
+  position fixed
+  top 0
+  right 0
+  bottom 0
+  left 0
+  background-color #f0f2f5
+  overflow hidden
+  display flex
+  justify-content center
+  align-items center
+</style>
