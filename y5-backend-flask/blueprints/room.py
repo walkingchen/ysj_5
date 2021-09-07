7b27707703baad79f0390357d4f59f3497e4f977
codingchan <codingchan@gmail.com>
2021-09-07 13:53:10 +0800

    fix bug
    Signed-off-by: codingchan <codingchan@gmail.com>

--- y5-backend-flask/blueprints/room.py
+++ y5-backend-flask/blueprints/room.py
@@ -367,8 +367,8 @@ def export_room_with_users():
         header = ['id', 'room_id', 'seat_no', 'user_id']
         csv_writer.writerow(header)
         for member in room_members:
-            line = ["'" + member.id + "'", "'" + member.room_id + "'",
-                    "'" + member.seat_no + "'", "'" + member.user_id + "'"]
+            line = ["'" + str(member.id) + "'", "'" + str(member.room_id) + "'",
+                    "'" + str(member.seat_no) + "'", "'" + str(member.user_id) + "'"]
             csv_writer.writerow(line)
 
         return send_from_directory('static', 'export_room_with_users', as_attachment=True)
