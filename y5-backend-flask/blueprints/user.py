5fee55c5721bf52a041f89d90db4c09f61b0c807
codingchan <codingchan@gmail.com>
2021-09-10 16:57:12 +0800

    fix bug
    Signed-off-by: codingchan <codingchan@gmail.com>

--- y5-backend-flask/blueprints/user.py
+++ y5-backend-flask/blueprints/user.py
@@ -38,7 +38,7 @@ def export_room():
     with open('static/export_user.csv', 'w',  encoding='UTF-8', newline='') as f:
         csv_writer = csv.writer(f)
         # header = ['id', 'user_id', 'room_type', 'room_id', 'seat_no', 'day', 'topic_no', 'message_id']
-        header = ['id', 'username', 'created_at', 'updated_at']
+        header = ['id', 'username', 'email', 'created_at', 'updated_at']
         csv_writer.writerow(header)
         for user in users:
             line = [str(user.id), str(user.username), str(user.email), str(user.created_at)]
