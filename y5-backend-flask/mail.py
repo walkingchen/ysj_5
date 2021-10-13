50c07185bb2dfa252f3bad606913b037602b9e67
codingchan <codingchan@gmail.com>
2021-10-14 00:42:40 +0800

    update
    Signed-off-by: codingchan <codingchan@gmail.com>

--- y5-backend-flask/mail.py
+++ y5-backend-flask/mail.py
@@ -15,3 +15,33 @@ def mail_notify(users):
                           sender=("Admin", "admin@soulfar.com"))
 
             conn.send(msg)
+
+
+def mail_morning(users):
+    with mail.connect() as conn:
+        for user in users:
+            if user.email is None:
+                continue
+            message = '...'
+            subject = "hello, %s" % user.username
+            msg = Message(recipients=[user.email],
+                          body=message,
+                          subject=subject,
+                          sender=("Admin", "admin@soulfar.com"))
+
+            conn.send(msg)
+
+
+def mail_night(users):
+    with mail.connect() as conn:
+        for user in users:
+            if user.email is None:
+                continue
+            message = '...'
+            subject = "hello, %s" % user.username
+            msg = Message(recipients=[user.email],
+                          body=message,
+                          subject=subject,
+                          sender=("Admin", "admin@soulfar.com"))
+
+            conn.send(msg)
