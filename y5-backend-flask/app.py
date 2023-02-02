a28d8aad7ae9ba67961a1160854992b44acac14a
CodingChan <codingchan@gmail.com>
2023-02-02 22:01:07 +0800

    debug = true
    Signed-off-by: CodingChan <codingchan@gmail.com>

--- y5-backend-flask/app.py
+++ y5-backend-flask/app.py
@@ -43,7 +43,6 @@ socketio.init_app(app, engineio_logger=True)
 socketio.on_namespace(RoomNamespace('/'))
 login_manager.init_app(app)
 
-
 db.init_app(app)
 
 scheduler.init_app(app)
@@ -226,4 +225,4 @@ with app.app_context():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
